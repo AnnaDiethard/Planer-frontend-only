00053262caf40a1b743dd20a06c2f5e5ea116afb
Annet <annadiethard@yandex.ru>
2024-06-04 14:52:21 +0300

    выделение выполненных задач, очистка списка задач

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,11 @@
 let tasks = []
+let doneTasks = []
 
 // рендер тудушника из LS
 if (localStorage.getItem('tasksList')) {
 	tasks = JSON.parse(localStorage.getItem('tasksList'))
-	tasks.forEach((tasksItem) => renderNewTask(tasksItem))
+	tasks.forEach((tasksItem) => renderTask(tasksItem))
+
 }
 
 // сохранение задач в LS
@@ -57,19 +59,56 @@ function addNewTaskToRunningList() {
     window.location.reload();
 }
 
-// рендер экземпляра задачи
-function renderNewTask(task) {
+// рендер экземпляра новой задачи
+function renderTask(task) {
     const runningListCard = document.querySelector("#runningListCard")
-	const taskHTML = `<li class="card-ul-item" id="${task.id}" style="display: flex; flex-direction: row; justify-content: space-between;">
+
+    if(task.status === "4") {
+        const taskHTML = `<li class="card-ul-item" id="${task.id}" style="display: flex; flex-direction: row; justify-content: space-between;">
                         <div>
-                            <input class="form-check-input" type="checkbox">
+                            <span class="runningList-icon-done"><i class="${task.icon} runningList-icon-done"></i></span>
+                            <label class="form-check-label-done" for="flexCheckDefault">${task.text}</label>
+                        </div>
+                    </li>`
+        runningListCard.insertAdjacentHTML('beforeend', taskHTML)
+    } else {
+        const taskHTML = `<li class="card-ul-item" id="${task.id}" style="display: flex; flex-direction: row; justify-content: space-between;">
+                        <div>
+                            <input class="form-check-input" type="checkbox" onclick="markTheTaskCompleted(this)">
                             <span class="runningList-icon"><i class="${task.icon}"></i></span>
                             <label class="form-check-label" for="flexCheckDefault">${task.text}</label>
                         </div>
                         <span><i class="fa-solid fa-trash" style="font-size: 14px; color: gray;" onclick="deleteTasksFromRunningList(this)"></i></span>
                     </li>`
+        runningListCard.insertAdjacentHTML('beforebegin', taskHTML)
+    }
+}
 
-    runningListCard.insertAdjacentHTML('beforeend', taskHTML)
+// выделение выполненных задач
+function markTheTaskCompleted(el) {
+    getTasksListFromLocalStorage()
+    const id = el.closest('li').id
+
+    tasks.forEach((task) => {
+        if(task.id == id) {
+            task.status = '4'
+        } 
+    })
+
+    saveTasksListInLocalStorage(tasks)
+    window.location.reload();
+}
+
+// очистка списка от выполненных задач
+function cleanTasksToRunningList() {
+    getTasksListFromLocalStorage()
+
+    tasks = tasks.filter(function(task) {
+        return task.status != '4'
+    })
+
+    saveTasksListInLocalStorage(tasks)
+    window.location.reload();
 }
 
 // удаление выбранной задачи
@@ -82,12 +121,11 @@ function deleteTasksFromRunningList(el) {
     tasks = tasks.filter(function(task) {
         return task.id != id
     })
-
-    saveTasksListInLocalStorage(tasks)
 }
 
 // очистка всего списка задач
 function deleteAllTaskToRunningList() {
     localStorage.removeItem('tasksList')
+    localStorage.removeItem('doneTasksList')
     window.location.reload();
 }
