8296b523e0a114d2a4c85105cfed7ecd09638c60
Annet <annadiethard@yandex.ru>
2024-06-26 19:17:17 +0300

    добила недельные виджеты

--- assets/elements.js
+++ assets/elements.js
@@ -1,4 +1,5 @@
-dragula([document.querySelector('#widgetsCol'), document.querySelector('.widget-col')        
+dragula([
+    document.getElementById('widgetsCol')
 ])
 
 // проверка для корректного отображения страницы при удалении всех виджетов
@@ -152,9 +153,8 @@ function renderWidget() {
         case undefined:
             widget = `<div></div>`
         case 'weekPlansTitleWidgetInput':
-            id = 'weekPlansTitleWidgetInput'
             widget = `<div class="col-12 widget-col">
-                            <div class="widget-everyWeekGoals" style="padding-bottom: 1rem">
+                            <div class="widget-everyWeekGoals" style="padding-bottom: 1rem" id="">
                                 <div class="card widget-card">
                                     <div class="card-header card-header-widget card-header__text">
                                         <div class="card-header-widget__block-header">
@@ -213,25 +213,25 @@ function renderWidget() {
                                         <div>
                                             <div class="tab-content" id="myTabsContent">
                                                 <div class="tab-pane fade show active" id="widget-everyWeekGoals-mon" role="tabpanel" aria-labelledby="widget-everyWeekGoals-mon-tab">
-                                                    <ul class="card-body__ul"</ul>
+                                                    <ul class="card-body__ul" data-day-name="monday"</ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-tue" role="tabpanel" aria-labelledby="widget-everyWeekGoals-tue-tab">
-                                                    <ul class="card-body__ul"</ul>
+                                                    <ul class="card-body__ul" data-day-name="tuesday"</ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-wed" role="tabpanel" aria-labelledby="widget-everyWeekGoals-wed-tab">
-                                                    <ul class="card-body__ul"></ul>
+                                                    <ul class="card-body__ul" data-day-name="wednesday"></ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-thu" role="tabpanel" aria-labelledby="widget-everyWeekGoals-thu-tab">
-                                                    <ul class="card-body__ul"></ul>
+                                                    <ul class="card-body__ul" data-day-name="thusday"></ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-fri" role="tabpanel" aria-labelledby="widget-everyWeekGoals-fri-tab">
-                                                    <ul class="card-body__ul"></ul>
+                                                    <ul class="card-body__ul" data-day-name="friday"></ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-sat" role="tabpanel" aria-labelledby="widget-everyWeekGoals-sat-tab">
-                                                    <ul class="card-body__ul"></ul>
+                                                    <ul class="card-body__ul" data-day-name="saturday"></ul>
                                                 </div>
                                                 <div class="tab-pane fade" id="widget-everyWeekGoals-sun" role="tabpanel" aria-labelledby="widget-everyWeekGoals-sun-tab">
-                                                    <ul class="card-body__ul"></ul>
+                                                    <ul class="card-body__ul" data-day-name="sunday"></ul>
                                                 </div>
                                             </div>
                                         </div>
@@ -338,7 +338,100 @@ function deleteAllTasksOnListWidget(el) {
 }
 
 // функции для виджета недельный планер
+const addTaskToEveryWeekGoalsWidgetDialog = document.querySelector('#everyWeekGoalWidgetDialog')
+let weekGoalsWidgetDialogTaskId = null
+let widget = null
+let taskToEveryWeekGoalItem = null
+
 function addTaskToEveryWeekGoalsWidget(el) {
-    console.log('add')
-    console.log('el', el)
+    // const title = document.querySelector('#everyWeekGoalWidgetModalTitleEdit')
+    // const button = document.querySelector('#editEveryWeekGoalBtn')
+    // title.style.display = 'none';
+    // button.style.display = 'none';
+    addTaskToEveryWeekGoalsWidgetDialog.showModal()
+
+    widget = el.closest('.widget-everyWeekGoals')
+    weekGoalsWidgetDialogTaskId = Date.now()
+    widget.id = weekGoalsWidgetDialogTaskId
+
+    return widget
+}
+
+function chooseWeekDay(el) {
+    const chooseDayName = el.textContent
+    const chooseDayNameID = el.getAttribute('data-day-name')
+    const shooseWeekDayNameLabel = document.querySelector('#weekDayNameLabel')
+    shooseWeekDayNameLabel.textContent = chooseDayName
+    shooseWeekDayNameLabel.setAttribute('data-day-name', chooseDayNameID)
+    shooseWeekDayNameLabel.style.color = '#313131'
+}
+
+function addTaskToEveryWeekGoalWidget() {
+    const dayName = document.querySelector('#everyWeekGoalWidgetDialog').querySelector('p').getAttribute('data-day-name')
+    console.log('dayName', dayName)
+    let taskValue = document.querySelector('#everyWeekGoalWidgetDialog').querySelector('.widget-list__input-text').value
+    console.log('taskValue', taskValue)
+    console.log('widget', widget)
+
+    const item = `<li class="widget-list__item card-ul-item" data-task-id="">
+                        <div class="task-item-block">
+                            <input type="checkbox" class="widget-list__item-checkbox" onclick="markTheTaskOfEveryWeekGoalWidgetCompleted(this)">
+                            <p class="widget-list__item-text">${taskValue}</p>
+                        </div>
+                        <div>
+                            <!-- <span ><i class="fa-solid fa-pencil card-body__btn-task-running-list" style="font-size: 14px;" onclick="editEveryWeekGoalOpenDialog(this)"></i></span> -->
+                            <span ><i class="fa-solid fa-trash card-body__btn-task-running-list" style="font-size: 14px;" onclick="deleteEveryWeekGoal(this)"></i></span>
+                        </div>
+                </li>`
+
+    const ulArr = widget.querySelector('.tab-content').querySelectorAll('ul')
+    console.log('ulArr', ulArr)
+    ulArr.forEach(el => {
+        if(el.getAttribute('data-day-name') == dayName) {
+            el.insertAdjacentHTML('beforeend', item)
+        }
+    })
+
+    taskValue = ''
+    saveWidgets()
+    
+    addTaskToEveryWeekGoalsWidgetDialog.close()
+    window.location.reload();
+}
+
+// TODO а оно вообще надо?
+// function editEveryWeekGoalOpenDialog(el) {
+//     const title = document.querySelector('#everyWeekGoalWidgetModalTitleAdd')
+//     const button = document.querySelector('#addEveryWeekGoalBtn')
+//     title.style.display = 'none';
+//     button.style.display = 'none';
+
+//     taskToEveryWeekGoalItem = el.closest('li')
+
+//     const taskDay = el.closest('ul').getAttribute('data-day-name')
+//     const taskLabel = document.querySelector('#weekDayNameLabel')
+//     taskLabel.textContent = taskDay
+
+//     const taskText = el.closest('.card-ul-item').querySelector('p').textContent
+//     const taskInput = document.querySelector('#everyWeekGoalWidgetContent')
+//     taskInput.value = taskText
+
+//     addTaskToEveryWeekGoalsWidgetDialog.showModal()
+
+//     return taskToEveryWeekGoalItem
+// }
+
+// function editTaskToEveryWeekGoalWidget() {
+//     console.log('edit')
+//     console.log('taskToEveryWeekGoalItem', taskToEveryWeekGoalItem)
+//     const item = taskToEveryWeekGoalItem
+//     const dayName = document.querySelector('#everyWeekGoalWidgetDialog').querySelector('p').getAttribute('data-day-name')
+//     const taskValue = document.querySelector('#everyWeekGoalWidgetDialog').querySelector('.widget-list__input-text').value
+//     item.querySelector('p').textContent = taskValue
+// }
+
+function deleteEveryWeekGoal(el) {
+    el.closest('li').remove()
+    saveWidgets()
+    window.reload()
 }
